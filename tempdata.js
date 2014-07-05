76688654d268cc16f6bce4f77a3a0fe3e9878ec1
a <aramology@gmail.com>
2014-07-05 12:25:09 -0500

    basic front-end

---
+++ tempdata.js
@@ -0,0 +1,177 @@
+			var user_arr = [
+				{
+					"organization":"Organization",
+					/**/
+					"username":"Journalist #0",
+					"email":"mail@journalist.com",
+					"password":"password"
+				},
+				{
+					"organization":"Organization",
+					/**/
+					"username":"Journalist #1",
+					"email":"mail@journalist.com",
+					"password":"password"
+				},
+				{
+					"organization":"Organization",
+					/**/
+					"username":"Journalist #2",
+					"email":"mail@journalist.com",
+					"password":"password"
+				},
+				{
+					"documents":[5,6,11],
+					"projects":[3,5],
+					"suggested":[2],
+					"other":[1,4],
+					/**/
+					"username":"Scriber #3",
+					"email":"mail@scriber.com",
+					"password":"password"
+				},
+				{
+					"documents":[],
+					"projects":[],
+					"suggested":[],
+					"other":[1,4],
+					/**/
+					"username":"Scriber #4",
+					"email":"mail@scriber.com",
+					"password":"password"
+				},
+				{
+					"documents":[],
+					"projects":[],
+					"suggested":[],
+					"other":[1,4],
+					/**/
+					"username":"Scriber #5",
+					"email":"mail@scriber.com",
+					"password":"password"
+				}
+			];
+			var project_arr = [
+				{
+					"journalist":2,
+					"start_date":"01/01/2000",
+					"kill_date":"12/31/2020",
+					"tag":"health",
+					"name":"Project #0",
+					"description":"description description description",
+					"stories":["","",""],
+					"ongoing":true,
+					"fields":["","",""],
+					"documents":[]
+				},
+				{
+					"journalist":2,
+					"start_date":"01/01/2000",
+					"kill_date":"12/31/2020",
+					"tag":"politics",
+					"name":"Project #1",
+					"description":"description description description",
+					"stories":["","",""],
+					"ongoing":true,
+					"fields":["","",""],
+					"documents":[]
+				},
+				{
+					"journalist":2,
+					"start_date":"01/01/2000",
+					"kill_date":"12/31/2020",
+					"tag":"business",
+					"name":"Project #2",
+					"description":"description description description",
+					"stories":["","",""],
+					"ongoing":true,
+					"fields":["","",""],
+					"documents":[]
+				},
+				{
+					"journalist":2,
+					"start_date":"01/01/2000",
+					"kill_date":"12/31/2020",
+					"tag":"technology",
+					"name":"Project #3",
+					"description":"description description description",
+					"stories":["","",""],
+					"ongoing":true,
+					"fields":["","",""],
+					"documents":[]
+				},
+				{
+					"journalist":2,
+					"start_date":"01/01/2000",
+					"kill_date":"12/31/2020",
+					"tag":"environment",
+					"name":"Project #4",
+					"description":"description description description",
+					"stories":["","",""],
+					"ongoing":true,
+					"fields":["","",""],
+					"documents":[]
+				},
+				{
+					"journalist":2,
+					"start_date":"01/01/2000",
+					"kill_date":"12/31/2020",
+					"tag":"education",
+					"name":"Project #5",
+					"description":"description description description",
+					"stories":["","",""],
+					"ongoing":true,
+					"fields":["","",""],
+					"documents":[]
+				}
+			];
+			var document_arr = [
+				{
+					"project":0,
+					"transcribed":false
+				},
+				{
+					"project":0,
+					"transcribed":false
+				},
+				{
+					"project":1,
+					"transcribed":false
+				},
+				{
+					"project":1,
+					"transcribed":false
+				},
+				{
+					"project":2,
+					"transcribed":false
+				},
+				{
+					"project":2,
+					"transcribed":false
+				},
+				{
+					"project":3,
+					"transcribed":false
+				},
+				{
+					"project":3,
+					"transcribed":false
+				},
+				{
+					"project":4,
+					"transcribed":false
+				},
+				{
+					"project":4,
+					"transcribed":false
+				},
+				{
+					"project":5,
+					"transcribed":false
+				},
+				{
+					"project":5,
+					"transcribed":false
+				}
+			];
